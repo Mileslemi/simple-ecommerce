b3d4b29d8f456312ca9e35e80b51732a9fbe113b
Miles Lemi <mileslemi@gmail.com>
2024-02-12 16:52:49 +0300

    _displayed products from a local js file.

---
+++ src/assets/files/products.js
@@ -0,0 +1,59 @@
+import product1 from "../images/1.png";
+import product2 from "../images/2.png";
+import product3 from "../images/3.png";
+import product4 from "../images/4.png";
+import product5 from "../images/5.png";
+import product6 from "../images/6.webp";
+import product7 from "../images/7.webp";
+import product8 from "../images/8.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Iphone",
+    price: 70000.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro",
+    price: 158000.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Canon",
+    price: 35000.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Denim Jacket",
+    price: 1799.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "LED lights",
+    price: 3000.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Don't Trip",
+    price: 499.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Golf T-Shirt",
+    price: 799.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Ngepa",
+    price: 399.0,
+    productImage: product8,
+  },
+];
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { PRODUCTS } from "../../assets/files/products";
+import Product from "./product";
+import "./shop.css";
 
 const Shop = () => {
-  return <div>shop</div>;
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h2>MilesCorp Shop</h2>
+      </div>
+      <div className="products">
+        {PRODUCTS.map((product) => (
+          <Product key={product.id} data={product} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
