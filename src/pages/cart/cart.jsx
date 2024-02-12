45b3ac421090ebe136ecc96c01f3b06516acd70d
Miles Lemi <mileslemi@gmail.com>
2024-02-12 17:42:34 +0300

    _displayed cart items

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { PRODUCTS } from "../../assets/files/products";
+import { ShopContext } from "../../context/shopContext";
+import CartItem from "./cartItem";
+import "./cart.css";
 
 const Cart = () => {
-  return <div>cart</div>;
+  const { cartItems, addToCart, removeFromCart } = useContext(ShopContext);
+  return (
+    <div className="cart">
+      <div>
+        <h2>Your Cart Items</h2>
+      </div>
+      <div className="cartItems">
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] > 0) {
+            return <CartItem key={product.id} data={product} />;
+          }
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
